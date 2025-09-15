eb813467795a8ef61d8f47c6671f1133517960c7
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2025-09-15 19:22:39 +0000

    teste vercel

--- the-pokedex/app/page.js
+++ the-pokedex/app/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.home}>
-      <h1>Bem-vindo à Pokédex!</h1>
+      <h1>Bem-vindo à Pokédex</h1>
       <Link href="/pokedex">Ir para Pokédex</Link>
     </main>
   );
