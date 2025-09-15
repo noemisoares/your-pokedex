4c6a5bf050024c28d687a8d1ba4b13db4ae074df
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2025-09-15 22:26:23 +0000

    teste de imagem pagina inicial

--- the-pokedex/app/page.js
+++ the-pokedex/app/page.js
@@ -6,6 +6,18 @@ export default function Home() {
   return (
     <main className={styles.home}>
       <h1>Bem-vindo à Pokédex</h1>
+      <p>
+        Explore informações sobre todos os Pokémon, descubra tipos, habilidades
+        e muito mais!
+      </p>
+      <div className={styles.banner}>
+        <Image
+          src="https://pm1.aminoapps.com/6434/a5f7322cdf21c4de36c7e2c48c926e4c433fe5e4_hq.jpg"
+          alt="Pikachu"
+          width={200}
+          height={200}
+        />
+      </div>
       <Link href="/pokedex">Ir para Pokédex</Link>
     </main>
   );
