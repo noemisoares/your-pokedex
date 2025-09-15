67940fd1dff693d16a1bee9f067420116875ae64
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2025-09-15 22:40:19 +0000

    teste de cor de fundo

--- the-pokedex/app/page.js
+++ the-pokedex/app/page.js
@@ -6,10 +6,12 @@ export default function Home() {
   return (
     <main className={styles.home}>
       <h1>Bem-vindo à Pokédex</h1>
+
       <p>
         Explore informações sobre todos os Pokémon, descubra tipos, habilidades
         e muito mais!
       </p>
+
       <div className={styles.banner}>
         <img
         src="https://pm1.aminoapps.com/6434/a5f7322cdf21c4de36c7e2c48c926e4c433fe5e4_hq.jpg"
@@ -18,7 +20,9 @@ export default function Home() {
         height="200"
         />
     </div>
+
       <Link href="/pokedex">Ir para Pokédex</Link>
+      
     </main>
   );
 }
